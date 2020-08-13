d65df5c8818c16ed1c951d058e2c4af62510c537
RonitNayak25 <ronitnayak25@gmail.com>
2020-08-13 23:11:31 +0530

    Frontend Ready For API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 import { CircularProgress } from "@material-ui/core";
 import "./App.css";
-import text from "./Prediction";
+import axios from 'axios';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   const [play, setPlay] = useState("");
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  async function handleSubmit (event){
+    event.preventDefault();
+    console.log(inputText);
+    setLoading(true);
+    setSuccess(false);
+    try{
+      const res = await axios.post(`http://0.0.0.0:5000/predict`, { "text": inputText });
+      console.log(res.data);
+      console.log(inputText);
+      setPlay(res.data.prediction);
+      setLoading(false);
+      setSuccess(true);
+    }
+    catch(e){
+      setLoading(false);
+      console.log(e)
+    }
+  }
+
+  const handleChange = event => {
+    setInputText(event.target.value);
+  }
+
+  let id = 0; // acts as a key for <p></p> mapping
 
   return (
     <div className="App">
@@ -14,14 +41,15 @@ function App() {
         <br />
         <div className="row">
           <div className="col-md-12">
-            <form class="form-group" method="POST">
+            <form className="form-group" method="POST" onSubmit={handleSubmit}>
               <input
                 type="text"
                 className="form-control"
                 id="text"
                 name="text"
+                onChange={handleChange}
               />
-              <button type="submit" class="btn btn-primary">
+              <button type="submit" className="btn btn-primary">
                 Submit
               </button>
             </form>
@@ -29,16 +57,16 @@ function App() {
             {loading ? (
               <CircularProgress color="secondary" />
             ) : (
-              <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                  <h1 class="display-4">
+              <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                  <h1 className="display-4">
                     <u>The Play</u>
                   </h1>
-                  <p class="lead" style={{ textAlign: "left" }}>
-                    {text.split("\n").map((i) => {
-                      return <p>{i}</p>;
+                  {success ? <div className="lead" style={{ textAlign: "left" }}>
+                    {play.split("\n").map((i) => {
+                      return <p key={id++}>{i}</p>;
                     })}
-                  </p>
+                  </div> : <div></div> }
                 </div>
               </div>
             )}
